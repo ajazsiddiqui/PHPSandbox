8168ad7969294882ebc4ac9071449e057bc0316b
Ajaz Siddiqui <ajazsiddiqui@mail.com>
2021-08-28 23:56:23 +0000

    Initial commit from PHPSandbox

---
+++ home.php
@@ -0,0 +1,438 @@
+<?php require_once('vendor/autoload.php');
+  $prices = [
+  0 => 
+  [
+	0 => 
+	[
+	  112 => 
+	  [
+		'startdate' => '2021/08/30',
+		'enddate' => '2021/08/31',
+		'base-rates' => 
+		[
+		  0 => 
+		  [
+			'plan_id' => 1,
+			'meal_plan' => 'American Plan (AP)',
+			'lesser_occupancy' => 3200,
+			'base_price' => 4000,
+			'extra_adult' => 800,
+			'extra_kids' => 800,
+		  ],
+		  1 => 
+		  [
+			'plan_id' => 2,
+			'meal_plan' => 'Continental Plan(CP)',
+			'lesser_occupancy' => 4200,
+			'base_price' => 5000,
+			'extra_adult' => 800,
+			'extra_kids' => 800,
+		  ],
+		  2 => 
+		  [
+			'plan_id' => 3,
+			'meal_plan' => 'European Plan (EP)',
+			'lesser_occupancy' => 5200,
+			'base_price' => 6000,
+			'extra_adult' => 800,
+			'extra_kids' => 800,
+		  ],
+		],
+		'bulk-rates' => 
+		[
+		  0 => 
+		  [
+			'plan_id' => 1,
+			'meal_plan' => 'American Plan (AP)',
+			'lesser_occupancy' => 3800,
+			'base_price' => 3000,
+			'extra_adult' => 600,
+			'extra_kids' => 600,
+		  ],
+		  1 => 
+		  [
+			'plan_id' => 2,
+			'meal_plan' => 'Continental Plan(CP)',
+			'lesser_occupancy' => 0,
+			'base_price' => 0,
+			'extra_adult' => 0,
+			'extra_kids' => 0,
+		  ],
+		  2 => 
+		  [
+			'plan_id' => 3,
+			'meal_plan' => 'European Plan (EP)',
+			'lesser_occupancy' => 0,
+			'base_price' => 0,
+			'extra_adult' => 0,
+			'extra_kids' => 0,
+		  ],
+		],
+		'contacted-rates' => 
+		[
+		  0 => 
+		  [
+			'plan_id' => 1,
+			'meal_plan' => 'American Plan (AP)',
+			'lesser_occupancy' => 3600,
+			'base_price' => 2800,
+			'extra_adult' => 400,
+			'extra_kids' => 400,
+		  ],
+		  1 => 
+		  [
+			'plan_id' => 2,
+			'meal_plan' => 'Continental Plan(CP)',
+			'lesser_occupancy' => 0,
+			'base_price' => 0,
+			'extra_adult' => 0,
+			'extra_kids' => 0,
+		  ],
+		  2 => 
+		  [
+			'plan_id' => 3,
+			'meal_plan' => 'European Plan (EP)',
+			'lesser_occupancy' => 0,
+			'base_price' => 0,
+			'extra_adult' => 0,
+			'extra_kids' => 0,
+		  ],
+		],
+		'promotional-rates' => 
+		[
+		  0 => 
+		  [
+			'plan_id' => 1,
+			'meal_plan' => 'American Plan (AP)',
+			'discount' => 10,
+		  ],
+		  1 => 
+		  [
+			'plan_id' => 2,
+			'meal_plan' => 'Continental Plan(CP)',
+			'discount' => 0,
+		  ],
+		  2 => 
+		  [
+			'plan_id' => 3,
+			'meal_plan' => 'European Plan (EP)',
+			'discount' => 0,
+		  ],
+		],
+	  ],
+	  113 => 
+	  [
+		'startdate' => '2021/08/30',
+		'enddate' => '2021/08/31',
+		'base-rates' => 
+		[
+		  0 => 
+		  [
+			'plan_id' => 1,
+			'meal_plan' => 'American Plan (AP)',
+			'lesser_occupancy' => 5000,
+			'base_price' => 6000,
+			'extra_adult' => 1000,
+			'extra_kids' => 1000,
+		  ],
+		  1 => 
+		  [
+			'plan_id' => 2,
+			'meal_plan' => 'Continental Plan(CP)',
+			'lesser_occupancy' => 6000,
+			'base_price' => 7000,
+			'extra_adult' => 1000,
+			'extra_kids' => 1000,
+		  ],
+		],
+		'bulk-rates' => 
+		[
+		  0 => 
+		  [
+			'plan_id' => 1,
+			'meal_plan' => 'American Plan (AP)',
+			'lesser_occupancy' => 0,
+			'base_price' => 0,
+			'extra_adult' => 0,
+			'extra_kids' => 0,
+		  ],
+		  1 => 
+		  [
+			'plan_id' => 2,
+			'meal_plan' => 'Continental Plan(CP)',
+			'lesser_occupancy' => 0,
+			'base_price' => 0,
+			'extra_adult' => 0,
+			'extra_kids' => 0,
+		  ],
+		],
+		'contacted-rates' => 
+		[
+		  0 => 
+		  [
+			'plan_id' => 1,
+			'meal_plan' => 'American Plan (AP)',
+			'lesser_occupancy' => 0,
+			'base_price' => 0,
+			'extra_adult' => 0,
+			'extra_kids' => 0,
+		  ],
+		  1 => 
+		  [
+			'plan_id' => 2,
+			'meal_plan' => 'Continental Plan(CP)',
+			'lesser_occupancy' => 0,
+			'base_price' => 0,
+			'extra_adult' => 0,
+			'extra_kids' => 0,
+		  ],
+		],
+		'promotional-rates' => 
+		[
+		  0 => 
+		  [
+			'plan_id' => 1,
+			'meal_plan' => 'American Plan (AP)',
+			'discount' => 0,
+		  ],
+		  1 => 
+		  [
+			'plan_id' => 2,
+			'meal_plan' => 'Continental Plan(CP)',
+			'discount' => 0,
+		  ],
+		],
+	  ],
+	],
+    1 => 
+	[
+	  112 => 
+	  [
+		'startdate' => '2021/08/30',
+		'enddate' => '2021/08/31',
+		'base-rates' => 
+		[
+		  0 => 
+		  [
+			'plan_id' => 1,
+			'meal_plan' => 'American Plan (AP)',
+			'lesser_occupancy' => 3200,
+			'base_price' => 4000,
+			'extra_adult' => 800,
+			'extra_kids' => 800,
+		  ],
+		  1 => 
+		  [
+			'plan_id' => 2,
+			'meal_plan' => 'Continental Plan(CP)',
+			'lesser_occupancy' => 4200,
+			'base_price' => 5000,
+			'extra_adult' => 800,
+			'extra_kids' => 800,
+		  ],
+		  2 => 
+		  [
+			'plan_id' => 3,
+			'meal_plan' => 'European Plan (EP)',
+			'lesser_occupancy' => 5200,
+			'base_price' => 6000,
+			'extra_adult' => 800,
+			'extra_kids' => 800,
+		  ],
+		],
+		'bulk-rates' => 
+		[
+		  0 => 
+		  [
+			'plan_id' => 1,
+			'meal_plan' => 'American Plan (AP)',
+			'lesser_occupancy' => 3800,
+			'base_price' => 3000,
+			'extra_adult' => 600,
+			'extra_kids' => 600,
+		  ],
+		  1 => 
+		  [
+			'plan_id' => 2,
+			'meal_plan' => 'Continental Plan(CP)',
+			'lesser_occupancy' => 0,
+			'base_price' => 0,
+			'extra_adult' => 0,
+			'extra_kids' => 0,
+		  ],
+		  2 => 
+		  [
+			'plan_id' => 3,
+			'meal_plan' => 'European Plan (EP)',
+			'lesser_occupancy' => 0,
+			'base_price' => 0,
+			'extra_adult' => 0,
+			'extra_kids' => 0,
+		  ],
+		],
+		'contacted-rates' => 
+		[
+		  0 => 
+		  [
+			'plan_id' => 1,
+			'meal_plan' => 'American Plan (AP)',
+			'lesser_occupancy' => 3600,
+			'base_price' => 2800,
+			'extra_adult' => 400,
+			'extra_kids' => 400,
+		  ],
+		  1 => 
+		  [
+			'plan_id' => 2,
+			'meal_plan' => 'Continental Plan(CP)',
+			'lesser_occupancy' => 0,
+			'base_price' => 0,
+			'extra_adult' => 0,
+			'extra_kids' => 0,
+		  ],
+		  2 => 
+		  [
+			'plan_id' => 3,
+			'meal_plan' => 'European Plan (EP)',
+			'lesser_occupancy' => 0,
+			'base_price' => 0,
+			'extra_adult' => 0,
+			'extra_kids' => 0,
+		  ],
+		],
+		'promotional-rates' => 
+		[
+		  0 => 
+		  [
+			'plan_id' => 1,
+			'meal_plan' => 'American Plan (AP)',
+			'discount' => 10,
+		  ],
+		  1 => 
+		  [
+			'plan_id' => 2,
+			'meal_plan' => 'Continental Plan(CP)',
+			'discount' => 0,
+		  ],
+		  2 => 
+		  [
+			'plan_id' => 3,
+			'meal_plan' => 'European Plan (EP)',
+			'discount' => 0,
+		  ],
+		],
+	  ],
+	  113 => 
+	  [
+		'startdate' => '2021/08/30',
+		'enddate' => '2021/08/31',
+		'base-rates' => 
+		[
+		  0 => 
+		  [
+			'plan_id' => 1,
+			'meal_plan' => 'American Plan (AP)',
+			'lesser_occupancy' => 5000,
+			'base_price' => 6000,
+			'extra_adult' => 1000,
+			'extra_kids' => 1000,
+		  ],
+		  1 => 
+		  [
+			'plan_id' => 2,
+			'meal_plan' => 'Continental Plan(CP)',
+			'lesser_occupancy' => 6000,
+			'base_price' => 7000,
+			'extra_adult' => 1000,
+			'extra_kids' => 1000,
+		  ],
+		],
+		'bulk-rates' => 
+		[
+		  0 => 
+		  [
+			'plan_id' => 1,
+			'meal_plan' => 'American Plan (AP)',
+			'lesser_occupancy' => 0,
+			'base_price' => 0,
+			'extra_adult' => 0,
+			'extra_kids' => 0,
+		  ],
+		  1 => 
+		  [
+			'plan_id' => 2,
+			'meal_plan' => 'Continental Plan(CP)',
+			'lesser_occupancy' => 0,
+			'base_price' => 0,
+			'extra_adult' => 0,
+			'extra_kids' => 0,
+		  ],
+		],
+		'contacted-rates' => 
+		[
+		  0 => 
+		  [
+			'plan_id' => 1,
+			'meal_plan' => 'American Plan (AP)',
+			'lesser_occupancy' => 0,
+			'base_price' => 0,
+			'extra_adult' => 0,
+			'extra_kids' => 0,
+		  ],
+		  1 => 
+		  [
+			'plan_id' => 2,
+			'meal_plan' => 'Continental Plan(CP)',
+			'lesser_occupancy' => 0,
+			'base_price' => 0,
+			'extra_adult' => 0,
+			'extra_kids' => 0,
+		  ],
+		],
+		'promotional-rates' => 
+		[
+		  0 => 
+		  [
+			'plan_id' => 1,
+			'meal_plan' => 'American Plan (AP)',
+			'discount' => 0,
+		  ],
+		  1 => 
+		  [
+			'plan_id' => 2,
+			'meal_plan' => 'Continental Plan(CP)',
+			'discount' => 0,
+		  ],
+		],
+	  ],
+	],
+  ],
+];
+
+//merge base rates and bulk rates
+$averagePrices = [];
+    foreach ($prices as $price){
+        foreach ($price as $pk => $pv){
+            foreach ($pv as $key => $room){
+                foreach($room['bulk-rates'] as $k => $v){
+                        $averagePrices[$key][$pk][$k] = ($v['base_price'] <= 0)?$room['base-rates'][$k]['base_price']:$v['base_price'];
+                }
+            }
+        }
+    }
+print_r($averagePrices);
+
+$a = [];
+foreach($averagePrices as $key => $ap){
+    
+        foreach($ap as $plans){
+            foreach($plans as $k => $p){
+                $a[$key][$k] = isset($a[$key][$k])? $a[$key][$k] + $plans[$k]: $plans[$k];
+            }
+        }
+       
+    
+}
+print_r( $a);
+
+
